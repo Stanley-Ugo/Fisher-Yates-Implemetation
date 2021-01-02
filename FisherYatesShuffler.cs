27d52c94e91461492026743ba5083b1a10db3b20
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-01-03 00:13:08 +0100

    The Implementations of FisherYatesShuffler Algorithms

---
+++ FisherYatesShuffler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public static class FisherYatesShuffler
+    {
+        private static Random randomNumber = new Random();
+
+        public static void DoFisherYatesShuffle(this object[] objects)
+        {
+            for (int i = objects.Length - 1; i > 0; i--)
+            {
+                int j = GetRandomNumberAtPositionZeroAndI(i);
+                objects.SwappValuesAtIndices(i, j);
+            }
+        }
+
+        public static void DoFisherYatesAlternativeShuffle(this object[] objects)
+        {
+            for (int i = 0; i < objects.Length - 2; i++)
+            {
+                int j = GetRandomNumberAtPositionZeroAndI((objects.Length - i) - 1);
+                objects.SwappValuesAtIndices(i, i + j);
+            }
+        }
+
+        private static int GetRandomNumberAtPositionZeroAndI(int i)
+        {
+            return randomNumber.Next(i + 1);
+        }
+    }
+}
---
+++ Swapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public static class Swapper
+    {
+        public static void SwappValuesAtIndices(this object[] objects, int i, int j)
+        {
+            object temp = objects[i];
+            objects[i] = objects[j];
+            objects[j] = temp;
+        }
+    }
+}
